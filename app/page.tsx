76eafc414659d488009f27dff2f3774c63c70257
Khadija Noor <dev.khadijanoor@gmail.com>
2025-07-01 23:16:44 -0700

    feat: add base app layout and global styles.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function LaslesVPNLanding() {
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="w-full flex justify-center bg-transparent">
+        <div className="w-full rounded-[16px] bg-gradient-to-b from-[rgba(248,248,248,1)] to-[rgba(248,248,248,0)] px-4 lg:px-8 pt-16 lg:pt-24 pb-0"></div>
+      </div>
+    </div>
+  );
+}
